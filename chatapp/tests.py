82c9460d6ed59feda27a738c94510f44c372834d
abdultruth <kolawole.abdulahi@gmail.com>
2023-12-06 21:28:19 +0100

    2nd commit01

--- chatapp/tests.py
+++ chatapp/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.urls import reverse 
+
+
+from .models import Room
 
 # Create your tests here.
+def create_chat(room_name):
+   return Room.objects.get_or_create(name=room_name)
+
+
+
+class RoomViewTests(TestCase):
+   def test_no_repeat_rooms(self):
+      """
+      If no questions exist, an appropriate message is displayed.
+      """
+      response = self.client.get(reverse('chat/room_name'))
+
+      self.assertEqual(response, "No Rooms are available.")
+      self.assertContains(response, "No Rooms are available.")
+      self.assertQuerysetEqual(response.context['room'], [])
+
+   def test_past_room(self):
+      """
+        Room with a created_at in the past are displayed on the
+        index page.
+      """
+
+      room = create_chat(room_name="room_name")
+      response = self.client.get(reverse('chat/room_name'))
+      self.assertQuerySetEqual(
+         response.context['room_name'],
+      )
+
+      def test_future_room(self):
+         pass
--- chatapp/views.py
+++ chatapp/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+
 
 
 from .models import Room
 
 # Create your views here.
-def index(request):
+def index(request) -> str:
     return render(request, "chatapp/index.html")
 
 
+@login_required(login_url='login')
 def room(request, room_name) -> str:
-    room = Room.objects.get_or_create(name=room_name)
-    if room:
+    if request.user:
+        room = Room.objects.get_or_create(name=room_name)
         return render(request, "chatapp/room.html", {"room_name": room_name})
+    else:
+        return redirect('login')
